50782510ce6c33d937c544ceb019d7638f04172d
tr-422 <talharasool196@gmail.com>
2026-03-04 05:18:18 +0500

    remove uncessary encoding and decoding of checkpoint

--- flutter/lib/core/models/sync_checkpoint.dart
+++ flutter/lib/core/models/sync_checkpoint.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 class SyncCheckpoint {
   final BigInt lastSyncedId;
   final String lastSyncedAt;
@@ -8,25 +6,6 @@ class SyncCheckpoint {
     required this.lastSyncedId,
     required this.lastSyncedAt,
   });
-
-  /// Encode checkpoint to opaque cursor string
-  String encode() {
-    final jsonMap = {
-      'lastSyncedId': lastSyncedId.toString(),
-      'lastSyncedAt': lastSyncedAt,
-    };
-    return base64Encode(utf8.encode(jsonEncode(jsonMap)));
-  }
-
-  /// Decode from cursor string
-  factory SyncCheckpoint.fromCursor(String cursor) {
-    final decoded = utf8.decode(base64Decode(cursor));
-    final map = jsonDecode(decoded) as Map<String, dynamic>;
-    return SyncCheckpoint(
-      lastSyncedId: BigInt.parse(map['lastSyncedId'] as String),
-      lastSyncedAt: map['lastSyncedAt'] as String,
-    );
-  }
 
   Map<String, dynamic> toJson() => {
         'lastSyncedId': lastSyncedId.toString(),
